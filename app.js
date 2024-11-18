0aa4483db9ca437866e82a6bd7172b42e18f5152
Sarah Dahmani <saarah.dahmani@hotmail.fr>
2024-11-18 19:45:15 +0100

    Création dépendances / models schémas

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://saaraahdahmani:<Fvz_86dzuRPdXF7>@cluster0.wplqs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+  const app = express();
+
+app.use((req, res, next) => {
+  console.log('Requête reçue !');
+  next();
+});
+
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.json({ message: 'Votre requête a bien été reçue !' });
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('Réponse envoyée avec succès !');
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,15 +1,47 @@
-//import du package http de Node
 const http = require('http');
+const app = require('./app');
 
+const normalizePort = val => {
+  const port = parseInt(val, 10);
 
-//fonction appelée à chaque requête avec 2 paramètres
-//req = requete et res = response
-//utilisation de la méthode end pour renvoyer une réponse string
-const server = http.createServer((req, res) => {
-    res.end('Voilà la réponse du serveur');
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT || '3002');
+app.set('port', port);
+
+const errorHandler = error => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const server = http.createServer(app);
+
+server.on('error', errorHandler);
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
 });
 
-//le serveur doit écouter/attendre les requêtes 
-//méthode listen avec le numéro du port
-//process.env.PORT est utilisé si le port 3000 n'est pas disponible
-server.listen(process.env.PORT || 3000);
+server.listen(port);
